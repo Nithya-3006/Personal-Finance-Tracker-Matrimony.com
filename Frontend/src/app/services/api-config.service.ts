b777cb8e642492e95b584731cf38208ea4ab4743
unknown <rpmegavarsitaadg@gmail.com>
2025-04-10 23:49:51 +0530

    Modified the contents in api-config service to get url from environment.ts -mega

--- Frontend/src/app/services/api-config.service.ts
+++ Frontend/src/app/services/api-config.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
-
+import { environment } from '../../enviroment/environment'
 @Injectable({
   providedIn: 'root'
 })
 export class ApiConfigService {
-
+  private baseUrl= environment.apiUrl;
   constructor() { }
+  getBaseUrl() {
+    return `${this.baseUrl}`;
+  }
 }
