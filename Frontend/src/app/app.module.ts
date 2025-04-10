bf42e3335bb8c8c871442530544ff6fda317657a
unknown <rpmegavarsitaadg@gmail.com>
2025-04-10 21:26:07 +0530

    Added files required for expenses (components , services) -mega

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ExpensesComponent } from './components/expenses/expenses.component';
+import { EditExpenseComponent } from './components/expenses/edit-expense/edit-expense.component';
+import { ExpenseDetailsComponent } from './components/expenses/expense-details/expense-details.component';
+import { ExpenseFormComponent } from './components/expenses/expense-form/expense-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExpensesComponent,
+    EditExpenseComponent,
+    ExpenseDetailsComponent,
+    ExpenseFormComponent
   ],
   imports: [
     BrowserModule,
